01bef666406810e36fb0afb2fb27f3b7ecf88088
iannakim <dear.hyunji@gmail.com>
2021-02-10 18:39:18 -0500

    Create input element for form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,32 +2,57 @@ import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEmailValid: false,
-            isNameValid: false,
-            isPhoneValid: false,
-            isUrlValid: false,
-        };
-
-    }
     
+    state = {
+        name: "",
+        email: "",
+        phone: "",
+        url: ""
+    }
+
+    handleChange = (evt) => {
+        this.setState({
+          [evt.target.name] : evt.target.value
+        })
+      }
+    
+
     render() {
         return (
             <div className="row">
             <h1 className="text-center">Form Validation</h1>
-            <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
+
+            <form className="form" onSubmit={this.handleSubmit}>
+                <p><input type="text"
+                        id="name"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        name="name"
+                        placeholder="Enter your name"
+                    /></p>
+                  <p><input type="text"
+                      id="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      name="email"
+                      placeholder="Enter your email address"
+                  /></p>
+                  <p><input type="text"
+                      id="phone"
+                      value={this.state.phone}
+                      onChange={this.handleChange}
+                      name="phone"
+                      placeholder="Enter your phone number"
+                  /></p>
+                <p><input type="text"
+                      id="url"
+                      value={this.state.phone}
+                      onChange={this.handleChange}
+                      name="url"
+                      placeholder="Enter your blog URL"
+                  /></p>
                 <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
+                    <a type="submit" href="#" className="button success expand round text-center">Verify</a>
                 </div>
             </form>
         </div>);
